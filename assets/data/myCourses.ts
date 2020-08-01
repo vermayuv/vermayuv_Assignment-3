3f46b7a706c4465db39bd13ae2fb794b70d7b64a
vermayuv <50668916+vermayuv@users.noreply.github.com>
2020-07-31 21:44:33 -0400

    Add files via upload

---
+++ assets/data/myCourses.ts
@@ -0,0 +1,9 @@
+//YUVRAJ VERMA vermayuv 991549287
+import {Courses} from '../../app/courseInfo';
+
+export const MYCOURSES : Courses[] = [
+    {courseCode:"SYST24444", courseTitle:"Mobile Web-Based Application", day:"Friday", professor:"Andy Pak"},
+    {courseCode:"PROG32356", courseTitle:".Net Technologies using C#", day:"Monday", professor:"Abdullah Abdullah"},
+    {courseCode:"PROG20799", courseTitle:"Data Structures Development C", day:"Thursday", professor:"George Fiel"},
+    {courseCode:"DBAS32100", courseTitle:"Database Management", day:"Wednesday", professor:"Humaira Siddiqui"}
+];
